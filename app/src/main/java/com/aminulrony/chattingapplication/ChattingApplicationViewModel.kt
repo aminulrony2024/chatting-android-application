7d4aff8786e576d6f5ebc747dd453a20e0deeac7
aminulrony2024 <aminulrony2024@gmail.com>
2024-11-07 12:50:50 +0600

    view model created

---
+++ app/src/main/java/com/aminulrony/chattingapplication/ChattingApplicationViewModel.kt
@@ -0,0 +1,8 @@
+package com.aminulrony.chattingapplication
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class ChattingApplicationViewModel: ViewModel() {
+}
