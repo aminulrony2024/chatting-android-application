7b11a7ca33583d0bd4274c20c93ba0710b97380f
aminulrony2024 <aminulrony2024@gmail.com>
2024-11-07 12:38:06 +0600

    route for different screen created

--- app/src/main/java/com/aminulrony/chattingapplication/MainActivity.kt
+++ app/src/main/java/com/aminulrony/chattingapplication/MainActivity.kt
@@ -14,6 +14,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.aminulrony.chattingapplication.ui.theme.ChattingApplicationTheme
 
+sealed class DestinationScreen(var route : String){
+    object SignUp : DestinationScreen("signup")
+    object Login : DestinationScreen("login")
+    object Profile : DestinationScreen("profile")
+    object ChatList : DestinationScreen("chatList")
+    object SingleChat : DestinationScreen("singleChat/{chatId}"){
+        fun createRoute(id : String) = "singleChat/$id"
+    }
+    object StatusList : DestinationScreen("statusList")
+    object SingleStatus : DestinationScreen("singleStatus/{userId}"){
+        fun createRoute(userId : String) = "singleStatus/$userId"
+    }
+}
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
