c3e4950e3c48aa2953839eb813f3c686ed6f9e4e
aminulrony2024 <aminulrony2024@gmail.com>
2024-11-09 17:25:03 +0600

    default route set as signup screen

--- app/src/main/java/com/aminulrony/chattingapplication/MainActivity.kt
+++ app/src/main/java/com/aminulrony/chattingapplication/MainActivity.kt
@@ -8,11 +8,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.composable
 import com.aminulrony.chattingapplication.Screens.LoginScreen
+import com.aminulrony.chattingapplication.Screens.SignupScreen
 import com.aminulrony.chattingapplication.ui.theme.ChattingApplicationTheme
 
 sealed class DestinationScreen(var route : String){
@@ -45,6 +47,11 @@ class MainActivity : ComponentActivity() {
     }
     @Composable
     fun ChatAppNavigation(){
-        LoginScreen()
+        val navController = rememberNavController()
+        NavHost(navController = navController, startDestination = DestinationScreen.SignUp.route){
+            composable(DestinationScreen.SignUp.route){
+                SignupScreen()
+            }
+        }
     }
 }
--- app/src/main/java/com/aminulrony/chattingapplication/Screens/SignupScreen.kt
+++ app/src/main/java/com/aminulrony/chattingapplication/Screens/SignupScreen.kt
@@ -1,7 +1,9 @@
 package com.aminulrony.chattingapplication.Screens
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun SignupScreen() {
+    Text(text="I am from Sign Up screen")
 }
